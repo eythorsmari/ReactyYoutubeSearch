52a8d0bdd658c33ba9609f0520f362d660e003a9
Eyþór Smári Þórbjörnsson <eythorsmari1@gmail.com>
2018-01-22 21:28:09 +0000

    Adding project to github

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAZ3eIWYND1jwZ1kCEOxzk_-bJoqN9t6Yg';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create new component. This componenent should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('Manchester United');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+             });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={ selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }  
+}
+
+
+
+//Take this component generated HTML and put in on the page (in the dom)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
+
+
+
